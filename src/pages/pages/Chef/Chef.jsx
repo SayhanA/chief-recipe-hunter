01d26ecab586f6ddcbea50bb9ebcd83cffc648b1
SayhanA <sayhanahmed05@gmail.com>
2023-05-04 00:11:39 +0600

    blog question page added

--- src/pages/pages/Chef/Chef.jsx
+++ src/pages/pages/Chef/Chef.jsx
@@ -34,14 +34,14 @@ const Chef = () => {
                         <img className='absolute lg:w-[900px] bottom-0 lg:-left-72' src={`/public/chefImages/${chef}.png`} alt="" />
                     </div>
                     <div className='relative'>
-                        <img className='hidden lg:block absolute left-0' src="/public/styleimg/landing-slide-1-img-10.png" alt="" />
-                        <img className='hidden lg:block absolute bottom-0 left-40' src="/public/styleimg/landing-slide-1-img-3.png" alt="" />
-                        <img className='hidden lg:block absolute right-20 top-20' src="/public/styleimg/landing-slide-1-img-6.png" alt="" />
-                        <img className='hidden lg:block absolute top-[50%] left-20' src="/public/styleimg/landing-slide-1-img-7.png" alt="" />
-                        <img className='hidden lg:block absolute top-[30%] left-[350px] w-[200px]' src="/public/styleimg/nuts.png" alt="" />
-                        <img className='hidden lg:block absolute top-0 left-80 w-[200px]' src="/public/styleimg/nuts.png" alt="" />
-                        <img className='hidden lg:block absolute bottom-20 right-0 w-[200px]' src="/public/styleimg/nuts.png" alt="" />
-                        <img className='absolute w-[200px] lg:w-[450px] lg:-left-[400px] -top-52 lg:top-[30%]' src="/public/styleimg/welcome.png" alt="" />
+                        <img className='hidden lg:block absolute left-0' src="/styleimg/landing-slide-1-img-10.png" alt="" />
+                        <img className='hidden lg:block absolute bottom-0 left-40' src="/styleimg/landing-slide-1-img-3.png" alt="" />
+                        <img className='hidden lg:block absolute right-20 top-20' src="/styleimg/landing-slide-1-img-6.png" alt="" />
+                        <img className='hidden lg:block absolute top-[50%] left-20' src="/styleimg/landing-slide-1-img-7.png" alt="" />
+                        <img className='hidden lg:block absolute top-[30%] left-[350px] w-[200px]' src="/styleimg/nuts.png" alt="" />
+                        <img className='hidden lg:block absolute top-0 left-80 w-[200px]' src="/styleimg/nuts.png" alt="" />
+                        <img className='hidden lg:block absolute bottom-20 right-0 w-[200px]' src="/styleimg/nuts.png" alt="" />
+                        <img className='absolute w-[200px] lg:w-[450px] lg:-left-[400px] -top-52 lg:top-[30%]' src="/styleimg/welcome.png" alt="" />
                         <p className='border-b-2 ml-5 lg:pb-5 border-yellow-300 absolute lg:-left-[300px] -top-32 lg:top-[60%] text-white font-semi-bold text-2xl lg:text-4xl font-mono'>{chef}</p>
                         <p className=' absolute bottom-[240px] lg:-left-[300px] lg:bottom-[20%] text-white lg:text-lg px-3 lg:w-[500px] font-mono'>Hi, This is {chef}.I am working for {chefData.experience}years on this profession and crated {chefData.recipes}<sup>+</sup> recipes and i have speciality in ({chefData.dish}) dishes...</p>
                         {/* <p className='absolute bottom-0 -rotate-90 '><ReactStarsRating value={chefData.ratings} /></p> */}
