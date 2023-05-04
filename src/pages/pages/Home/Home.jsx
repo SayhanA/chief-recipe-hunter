9b27794bb0d41f6e8478db475eebcb93fb93ed3a
SayhanA <sayhanahmed05@gmail.com>
2023-05-04 23:39:14 +0600

    design added

--- src/pages/pages/Home/Home.jsx
+++ src/pages/pages/Home/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
 
             {/* <Banner /> */}
             <div className='md:mx-[10%] md:mt-20'>
-                <h3 className='text-4xl font-extrabold text-center font-mono'>Our {more ? "All" : "Famous"} Chefs</h3>
+                <h3 className='text-2xl lg:text-4xl font-extrabold text-center font-mono'>Our {more ? "All" : "Famous"} Chefs</h3>
                 {
                     more ?
                         <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 mt-5'>
--- src/pages/pages/Login/Login.jsx
+++ src/pages/pages/Login/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password)
+        // console.log(email, password)
 
         // // firebase login
         singIn(email, password)
